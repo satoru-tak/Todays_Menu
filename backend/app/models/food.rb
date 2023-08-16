565b3b9932ff9abdeddae29c19d821e47ee98b3a
satoru-tak <satoru.takaira@gmail.com>
2023-08-16 23:54:23 +0900

    Adding and initializing gem annotate

--- backend/app/models/food.rb
+++ backend/app/models/food.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: foods
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Food < ApplicationRecord
 end
